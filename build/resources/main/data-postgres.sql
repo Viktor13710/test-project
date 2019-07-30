6292610758f8df4be54eae73be845c288c7cd594
Victor Talpa <c-victor.talpa@edifecs.com>
2019-07-30 15:04:14 +0300

    added build folder to gitignore

--- build/resources/main/data-postgres.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Student(firstName, lastName, age, faculty, groupID)
-VALUES ('Ion', 'Moraru', 20, 'FCIM', 'TI-144');
-
-INSERT INTO Student(firstName, lastName, age, faculty, groupID)
-VALUES ('Andrei', 'Stratan', 22, 'FIMIT', 'IM-152');
-
-INSERT INTO Student(firstName, lastName, age, faculty, groupID)
-VALUES ('Alexandru', 'Puscas', 23, 'UA', 'A-161');
--- build/resources/main/schema-postgres.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS Student;
-CREATE TABLE Student (
-    id serial PRIMARY KEY,
-    firstName varchar(100) NOT NULL,
-    lastName varchar(100) NOT NULL ,
-    age INT NOT NULL,
-    faculty varchar(100) NOT NULL,
-    groupID varchar(100) NOT NULL
-);
